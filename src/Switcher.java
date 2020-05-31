cca3ed913342e802f7d065e110ef0716c5e3063e
Student52 <dmitry.gra4ev@gmail.com>
2020-05-31 16:53:56 +0300

    Переключатель программ.

--- src/Switcher.java
+++ src/Switcher.java
@@ -1,12 +1,18 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Выполнение переключения между программами Калькулятор / Массив слов
+ * @author Dmitriy Grachev
+ * @version 2.0
+ */
+
 public class Switcher {
     public static void main(String[] args) throws IOException {
         System.out.println("Введите число 1 (запуск калькулятора) или 2 (запуск поиска строки в массиве ");
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
-        switch (num) {
+        switch (num) { // на определенное число создается соотв-ий экземпляр класса и его метод main
             case 1:
                 Calculator yourClass1 = new Calculator();
                 yourClass1.main(args);
